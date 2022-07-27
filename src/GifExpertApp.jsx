6c2408c0cffa6fc14f22b80fd657f948476971da
ruprosperi2 <rubenjariasp@gmail.com>
2022-07-26 21:03:00 -0400

    feat: video 74: creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,9 +2,7 @@ import React, {useState} from 'react';
 
 const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch']);
-
-    console.log(categories)
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
     return (
         <>
@@ -14,6 +12,11 @@ const GifExpertApp = () => {
             {/*Input*/}
 
             {/*Listado de Git*/}
+            <ol>
+                {
+                    categories.map((category) => <li key={category}>{category}</li>)
+                }
+            </ol>
         </>
     );
 };
